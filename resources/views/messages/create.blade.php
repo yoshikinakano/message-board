789eb593d54017a99356117a42a1020a526b6b25
yoshiki.nakano <nakano.yoshiki@a-tm.co.jp>
2018-05-11 05:50:22 +0000

    bootstrap design

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -5,16 +5,24 @@
     <h1>メッセージ新規作成ページ</h1>
 
 
-    {!! Form::model($message, ['route' => 'messages.store']) !!}
+    <div class="row">
+        <div class="col-xs-6">
+            {!! Form::model($message, ['route' => 'messages.store']) !!}
 
-        {!! Form::label('title', 'タイトル:') !!}
-        {!! Form::text('title') !!}
+                <div class="form-group">
+                    {!! Form::label('title', 'タイトル:') !!}
+                    {!! Form::text('title', null, ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::label('content', 'メッセージ:') !!}
-        {!! Form::text('content') !!}
+                <div class="form-group">
+                    {!! Form::label('content', 'メッセージ:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
 
-        {!! Form::submit('投稿') !!}
+                {!! Form::submit('投稿', ['class' => 'btn btn-primary']) !!}
 
-    {!! Form::close() !!}
+            {!! Form::close() !!}
+        </div>
+    </div>
 
 @endsection
