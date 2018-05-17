ceca14e840678955b6f40a868d78cbe92d882469
yoshiki.nakano <nakano.yoshiki@a-tm.co.jp>
2018-05-17 04:18:31 +0000

    responsive design

--- resources/views/messages/create.blade.php
+++ resources/views/messages/create.blade.php
@@ -6,7 +6,7 @@
 
 
     <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::model($message, ['route' => 'messages.store']) !!}
 
                 <div class="form-group">
--- resources/views/messages/edit.blade.php
+++ resources/views/messages/edit.blade.php
@@ -5,7 +5,7 @@
     <h1>id: {{ $message->id }} のメッセージ編集ページ</h1>
 
    <div class="row">
-        <div class="col-xs-6">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
             {!! Form::model($message, ['route' => ['messages.update', $message->id], 'method' => 'put']) !!}
                 <div class="form-group">
                     {!! Form::label('title', 'タイトル:') !!}
